19fffdf46a73cfc4fe19d7a14e4c8d957cdb65af
Daan Verstraten <Huntondoom@gmail.com>
2019-12-23 11:55:26 +0100

    Updated

---
+++ ItemTemplates/Visual C#/DaanV2/Public/Partial Struct/Struct - Initialize.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace $rootnamespace$ {
+    ///DOLATER <summary> add description for struct: $fileinputname$</summary>
+	[Serializable, DataContract]
+    public partial struct $fileinputname$ {
+        /// <summary>Creates a new instance of <see cref="$fileinputname$"/></summary>
+        public $fileinputname$() {
+            //TODO Fill Constructor for struct: $fileinputname$
+        }
+    }
+}
---
+++ ItemTemplates/Visual C#/DaanV2/Public/Partial Struct/Struct - Properties.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace $rootnamespace$ {
+    public partial struct $fileinputname$ {
+		  //DOLATER add properties to struct $fileinputname$
+    }
+}
