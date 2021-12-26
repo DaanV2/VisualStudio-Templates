89aa3516ae01e658ce89af4ebaa5c8b912f69214
DaanV2 <daanverstraten@hotmail.com>
2021-12-26 16:42:45 +0100

    Added test class

---
+++ ItemTemplates/Visual C#/DaanV2/Test/Class/Class - Initialize.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace $rootnamespace$ {
+    ///DOLATER <summary>The test class $fileinputname$ that tests: </summary>
+	    public sealed partial class $fileinputname$ {
+        /// <summary>Creates a new instance of <see cref="$fileinputname$"/></summary>
+        public $fileinputname$() {
+
+        }
+    }
+}
---
+++ ItemTemplates/Visual C#/DaanV2/Test/Class/Class - Tests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace $rootnamespace$ {
+    [TestClass]
+    public sealed partial class $fileinputname$ {
+        [TestMethod]
+        public void Test1() {
+          //TODO add test code
+        }
+    }
+}
