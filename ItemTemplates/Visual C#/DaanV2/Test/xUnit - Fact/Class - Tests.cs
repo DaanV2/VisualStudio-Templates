768b11a22394e73286564d9b6c97b7a455e483e5
DaanV2 <daanverstraten@hotmail.com>
2023-05-17 15:15:11 +0200

    Updated paths

---
+++ ItemTemplates/Visual C#/DaanV2/Test/xUnit - Fact/Class - Tests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace $rootnamespace$;
+
+public sealed partial class $fileinputname$ {
+    [Fact(DisplayName = "Test1")]
+    public void Test1(Int32 param) {
+        //TODO add test code
+    }
+}
---
+++ ItemTemplates/Visual C#/DaanV2/Test/xUnit - Theory/Class - Tests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace $rootnamespace$;
+
+public sealed partial class $fileinputname$ {
+    [Theory(DisplayName = "Test1")]
+    [InlineData(1)]
+    public void Test1(Int32 param) {
+        //TODO add test code
+    }
+}
